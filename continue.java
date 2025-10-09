f783f6c482d06611b01456bce232a6f3753d2e90
Amaana16 <sherienshadali@gmail.com>
2025-10-09 14:32:38 +0000

    new

---
+++ continue.java
@@ -0,0 +1,11 @@
+public class ContinueExample{
+    public static void main(String[]args){
+        for(int i=1;i<=5;i++){
+            if(i==3){
+                System.out.println("skipping i="+i);
+                break;
+            }
+            System.out.println("i="+i);
+        }
+    }
+}
