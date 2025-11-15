ccc113574493ff3fd40f823e1c8c41be5e576a85
Amaana16 <sherienshadali@gmail.com>
2025-11-15 17:03:16 +0000

    new commit

--- foreach.java
+++
@@ -1,9 +0,0 @@
-[3:03 PM, 9/23/2025] Amaana: public class ForEachExample {
-    public static void main(String[] args) {
-        String[] fruits = {"Apple", "Banana", "Cherry"};
-        for (String fruit : fruits) {
-            System.out.println("Fruit: " + fruit);
-        }
-    }
-}
-[3:03 PM, 9/23/2025] Amaana: 
